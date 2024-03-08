f5c603f7b565711f6cb3ca819f636f1dbfb7f3e7
pritesh <pritesh.gmail.com>
2024-03-08 22:14:45 +0530

    Adding terraform files

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable region {
+  type        = string
+  default     = "us-east-1"
+  description = "This variable is for region"
+}
+ 
+
+variable s3_bucket {
+    type = string
+    default = "myterrrraformbucekt"
+}
